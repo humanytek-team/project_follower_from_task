ba5b3a2b4802751faee410145ee02f930876464c
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-10-07 15:57:20 -0500

    [INIT]

---
+++ models/project_task.py
@@ -0,0 +1,17 @@
+from odoo import api, fields, models, _
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+   
+    @api.model
+    def create(self, vals):
+        task = super(ProjectTask, self).create(vals)
+        task.sudo().message_follower_ids.unlink()
+        return task
+
+    @api.multi
+    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
+        res = super(ProjectTask, self).message_subscribe(partner_ids, channel_ids, subtype_ids)
+        self.project_id.message_subscribe(partner_ids=self.message_partner_ids.ids, subtype_ids=[1])
+        return res
