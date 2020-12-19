0450ff310b6b0795e53f30e057d7be132516aff1
Moisés Navarro <moisalejandro@gmail.com>
2020-12-19 10:15:16 -0600

    [FIX] recursive

--- models/project_task.py
+++ models/project_task.py
@@ -10,8 +10,11 @@ class ProjectTask(models.Model):
         task.sudo().message_follower_ids.unlink()
         return task
 
-    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
+    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None, project=True):
         res = super(ProjectTask, self).message_subscribe(partner_ids, channel_ids, subtype_ids)
-        for r in self:
-            r.project_id.message_subscribe(partner_ids=r.message_partner_ids.ids, subtype_ids=[1])
+        if not project:
+            for r in self:
+                r.project_id.message_subscribe(
+                    partner_ids=r.message_partner_ids.ids, subtype_ids=[1]
+                )
         return res
