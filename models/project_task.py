8742ff58834f7445c909c5f8856584965f514ec9
Moisés Navarro <moisalejandro@gmail.com>
2021-01-06 21:34:56 -0600

    [IMP]

--- models/project_task.py
+++ models/project_task.py
@@ -10,11 +10,25 @@ class ProjectTask(models.Model):
         task.sudo().message_follower_ids.unlink()
         return task
 
-    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None, project=True):
+    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
         res = super(ProjectTask, self).message_subscribe(partner_ids, channel_ids, subtype_ids)
-        if not project:
-            for r in self:
+        for r in self:
+            current_followers = (
+                self.env["mail.followers"]
+                .sudo()
+                .search(
+                    [
+                        ("res_model", "=", r.project_id._name),
+                        ("res_id", "in", r.project_id.id),
+                        "|",
+                        ("partner_id", "in", partner_ids or []),
+                        ("channel_id", "in", channel_ids or []),
+                    ]
+                )
+            )
+            partners_to_suscribe = r.message_partner_ids - current_followers
+            if partners_to_suscribe:
                 r.project_id.message_subscribe(
-                    partner_ids=r.message_partner_ids.ids, subtype_ids=[1]
+                    partner_ids=partners_to_suscribe.ids, subtype_ids=[1]
                 )
         return res
