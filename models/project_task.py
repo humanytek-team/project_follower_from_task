d76f126faad921f686fad73d91a2743bd8a51d56
Moises Navarro <moisalejandro@gmail.com>
2023-07-03 08:49:17 -0600

    [MIG] 13.0 -> 15.0

--- models/project_task.py
+++ models/project_task.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 
 
 class ProjectTask(models.Model):
@@ -10,11 +10,8 @@ class ProjectTask(models.Model):
         task.sudo().message_follower_ids.unlink()
         return task
 
-    def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None, project=True):
-        res = super(ProjectTask, self).message_subscribe(partner_ids, channel_ids, subtype_ids)
-        if not project:
-            for r in self:
-                r.project_id.message_subscribe(
-                    partner_ids=r.message_partner_ids.ids, subtype_ids=[1]
-                )
+    def message_subscribe(self, partner_ids=None, subtype_ids=None):
+        res = super(ProjectTask, self).message_subscribe(partner_ids, subtype_ids)
+        for r in self:
+            r.project_id.message_subscribe(partner_ids=r.message_partner_ids.ids, subtype_ids=[1])
         return res
