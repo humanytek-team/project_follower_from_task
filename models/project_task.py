7f3f419111c48cebc4c780724a54aa2effc7034d
Moises Navarro <moisalejandro@gmail.com>
2023-07-17 11:36:19 -0600

    [FIX]

--- models/project_task.py
+++ models/project_task.py
@@ -10,10 +10,12 @@ class ProjectTask(models.Model):
         task.sudo().message_follower_ids.unlink()
         return task
 
-    def message_subscribe(self, partner_ids=None, subtype_ids=None, project=True):
+    def message_subscribe(self, partner_ids=None, subtype_ids=None):
+        prev_inf_recursion = self.env.context.get("inf_recursion", False)
         res = super(ProjectTask, self).message_subscribe(partner_ids, subtype_ids)
-        if not project:
-            for r in self:
+        if not prev_inf_recursion:
+            self_ctx = self.with_context(inf_recursion=True)
+            for r in self_ctx:
                 r.project_id.message_subscribe(
                     partner_ids=r.message_partner_ids.ids, subtype_ids=[1]
                 )
