23b16aa3b535c2369d63ab4e104ef09e3e2b00c1
mbrehumanytek <matias@humanytek.com>
2024-11-28 18:12:28 +0000

    mig to 17.0

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Project Follower from task",
-    "version": "1.0",
+    "version": "17.0.1.0.0",
     "author": "Humanytek",
     "website": "http://humanytek.com",
     "depends": [
--- models/project_task.py
+++ models/project_task.py
@@ -6,12 +6,15 @@ class ProjectTask(models.Model):
 
     @api.model
     def create(self, vals):
+        # Crear la tarea usando super y luego eliminar seguidores
         task = super(ProjectTask, self).create(vals)
         task.sudo().message_follower_ids.unlink()
         return task
 
     def message_subscribe(self, partner_ids=None, subtype_ids=None):
+        # Revisar si estamos en una recursion infinita para evitar bucles
         prev_inf_recursion = self.env.context.get("inf_recursion", False)
+        # Ejecutar la lógica original de suscripción a mensajes
         res = super(ProjectTask, self).message_subscribe(partner_ids, subtype_ids)
         if not prev_inf_recursion:
             self_ctx = self.with_context(inf_recursion=True)
